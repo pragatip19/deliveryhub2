e752511b72732495222662180e80ea0c72745b68
pragati19 <pragati.patil@leucinetech.com>
2026-03-16 20:19:10 +0530

    Add getWeekNumber to workdays

--- src/lib/workdays.js
+++ src/lib/workdays.js
@@ -116,3 +116,14 @@ export function nextWorkday(date) {
   }
   return d;
 }
+
+/**
+ * Get the ISO week number for a date (1–53)
+ */
+export function getWeekNumber(date) {
+  const d = new Date(date);
+  d.setHours(0, 0, 0, 0);
+  d.setDate(d.getDate() + 3 - ((d.getDay() + 6) % 7));
+  const week1 = new Date(d.getFullYear(), 0, 4);
+  return 1 + Math.round(((d.getTime() - week1.getTime()) / 86400000 - 3 + ((week1.getDay() + 6) % 7)) / 7);
+}
